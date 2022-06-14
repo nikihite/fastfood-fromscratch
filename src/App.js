451697ff71bde55e5d1f9761c80644efe3896e1e
Niki Hite <nikihite@comcast.net>
2022-06-14 11:19:58 -0700

    App.js added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DrinkDropdown from './DrinkDropdown';
+import SideDropDown from './SideDropdown';
+import FoodDropDown from './FoodDropdown';
+import NameInput from './NameInput';
+import InstructionsForm from './InstructionForm';
+import FoodImages from './FoodImages';
 
 function App() {
+
+  const [foodId, setFoodId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [orderName, setOrderName] = useState('customer');
+  const [extraInstructions, setExtraInstructions] = useState(['extra sauce']); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='order-name'>
+        <h3> Order for {orderName}</h3>
+        <FoodImages
+          foodId={foodId}
+          sideId={sideId}
+          drinkId={drinkId} />
+      </div>
+
+      <div className='order-form'>
+        <FoodDropDown setFoodId={setFoodId} />
+        <DrinkDropdown setDrinkId={setDrinkId} />
+        <SideDropDown setSideId={setSideId} />
+        <NameInput setOrderName={setOrderName} />
+        <InstructionsForm
+          extraInstructions={extraInstructions} setExtraInstructions={setExtraInstructions} />
+      </div>
+      
+      <div className='instructions-list'>
+        {
+          extraInstructions.map((instruction, i) => <li key={`${instruction}=${i}`}>
+            {instruction}
+          </li>)
+        }
+      </div>
+
     </div>
   );
 }
